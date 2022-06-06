2e20a72049f5108202d5bfa20561658740f3f99d
yongjunism <yjcho.dev@gmail.com>
2022-06-06 22:02:01 +0900

    220606 yj 테케4

---
+++ 삼성 SW 역량 테스트 기출 문제(0606)/BOJ 14499 - 주사위 굴리기(골드4)/BOJ 14499.py
@@ -0,0 +1,41 @@
+import sys
+input = sys.stdin.readline
+
+n, m, x, y, k = map(int, input().split())
+board = []
+dice = [0] * 6
+dirs = [(1,0), (0,1), (0,-1), (-1,0)]
+
+for _ in range(n):
+    board.append(list(map(int, input().split())))
+
+def move(dir):
+    a, b, c, d, e, f = dice[0], dice[1], dice[2], dice[3], dice[4], dice[5]
+    if dir == 1:
+        dice[0], dice[1], dice[2], dice[3], dice[4], dice[5] = d, b, a, f, e, c
+    elif dir == 2:
+        dice[0], dice[1], dice[2], dice[3], dice[4], dice[5] = c, b, f, a, e, d
+    elif dir == 3:
+        dice[0], dice[1], dice[2], dice[3], dice[4], dice[5] = e, a, c, d, f, b
+    else:
+        dice[0], dice[1], dice[2], dice[3], dice[4], dice[5] = b, f, c, d, a, e
+
+action = list(map(int, input().split()))
+
+for dir in action:
+    dx, dy = dirs[dir % 4]
+    nx = x + dx
+    ny = y + dy
+    if 0 <= nx < n and 0 <= ny < m:
+        x, y = nx, ny
+    else:
+        # nx -= dx
+        # ny -= dy
+        continue
+    move(dir)
+    if board[x][y] == 0:
+        board[x][y] == dice[5]
+    else:
+        dice[5] = board[x][y]
+        board[x][y] == 0
+    print(dice[0])
