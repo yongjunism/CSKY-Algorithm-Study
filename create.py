0bd38c7b092eb4aa3ff650e4f4083b64ae2efd21
2riing <2riing2@gmail.com>
2022-06-07 02:29:55 +0900

    problem set and crawling code update

--- create.py
+++ create.py
@@ -1,12 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
-import os
 base = 'https://www.acmicpc.net/problem/'
-
+import os
 while True:
-    print('문제 번호를 입력하세요. 중단하려면 n을 입력하세요.')
+    print('문제 번호를 입력하세요. 중단하려면 1을 입력하세요.')
     num = input()
-    if num == 'n' or num == 'N':
+    if num == '1' or num == 'n':
         break
     url = base + num
     print(url)
@@ -20,13 +19,13 @@ while True:
         title = soup.find("span", id="problem_title").get_text()
         sample_i = soup.select("pre[id^=sample-input]")
 
-        os.system(f"mkdir \"[BOJ_{num}]{title}\"")
-        os.system(f"echo import sys > \"[BOJ_{num}]{title}/BOJ_{num}.py\"")
-        os.system(f"echo sys.stdin = open('input1.txt') >> \"[BOJ_{num}]{title}/BOJ_{num}.py\"")
+        os.system(f"mkdir \"BOJ {num} - {title}\"")
+        os.system(f"echo import sys > \"BOJ {num} - {title}/BOJ_{num}.py\"")
+        os.system(f"echo sys.stdin = open('input1.txt') >> \"BOJ {num} - {title}/BOJ_{num}.py\"")
 
         for i in range(len(sample_i)):
             for text_i in sample_i[i].text.strip().split('\n'):
-                os.system(f"echo {text_i} >> \"[BOJ_{num}]{title}/input{i+1}.txt\"")
+                os.system(f"echo {text_i} >> \"BOJ {num} - {title}/input{i+1}.txt\"")
 
     else:
         print(response.status_code)
