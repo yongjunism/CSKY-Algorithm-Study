db5a6293a8f4246996f55ac6c4276b2e827e89e3
yongjunism <yjcho.dev@gmail.com>
2022-07-02 00:07:29 +0900

    Rename 백준 -> BOJ

---
+++ BOJ 배열 돌리기(0627)/BOJ 16927 - 배열 돌리기 2/BOJ_16927.py
@@ -0,0 +1,5 @@
+import sys
+input = sys.stdin.readline
+
+n, m, r = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
---
+++ BOJ 배열 돌리기(0627)/BOJ 5556 - 타일/BOJ_5556.py
@@ -0,0 +1,22 @@
+import sys 
+input = sys.stdin.readline
+
+n = int(input())
+k = int(input())
+removed = [list(map(int, input().split())) for _ in range(k)]
+
+art = [[0]*n for _ in range(n)]
+
+# 주대각선
+for i in range(n):
+    if i % 3 == 1:
+        art[i][i] = 2
+    elif i % 3 == 2:
+        art[i][i] = 3
+    else:
+        art[i][i] = 1
+    
+art[i][i] = 
+# 부대각선
+
+# 3의
